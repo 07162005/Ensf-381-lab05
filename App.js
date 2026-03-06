3a082d1fb9f3738c170efb83f1300a474eab450d
Andrew <andrew.dang05@gmail.com>
2026-03-06 11:43:21 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // You can keep or remove this import depending on your needs
+
+function App() {
+  return (
+    <div>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+    </div>
+  );
+}
+
+export default App;
